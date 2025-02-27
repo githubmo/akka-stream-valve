4a36ca45d3804fa4aa0318f00b72ca73eac8d8e7
Mohammed Abdulrazeg <github@mzawi.me>
2025-02-27 12:41:57 +0400

    Fixing bugs and reducing features to improve thread safety

--- src/main/java/io/example/Valve.java
+++ src/main/java/io/example/Valve.java
@@ -1,5 +1,6 @@
 package io.example;
 
+import akka.Done;
 import akka.japi.Pair;
 import akka.stream.Attributes;
 import akka.stream.FlowShape;
@@ -30,9 +31,10 @@ public class Valve<T> extends AbstractGraphStageWithMaterializedValue<FlowShape<
     }
 
     // Feedback: this should be final since it is accessed from another thread
-    private boolean isPausedOnStart = false;
+    private final boolean isPausedOnStart;
 
     public Valve() {
+        isPausedOnStart = false;
     }
 
     public Valve(boolean isPausedOnStart) {
@@ -42,21 +44,10 @@ public class Valve<T> extends AbstractGraphStageWithMaterializedValue<FlowShape<
     @Override
     public Pair<GraphStageLogic, ValveControl> createLogicAndMaterializedValuePair(Attributes inheritedAttributes) {
 
-        // Feedback: the only two things that need thread-safety/synchronization/atomics
-        // is signalling that the stage should be paused or unpaused
-        // and possibly to inspect that it is paused/unpaused, however, since the latter is
-        // idempotent: pausing an already paused stream, unpausing an unpaused stream the latter may not be needed.
-        // So if you do not stricly need the isPaused method on the valve, skipping this and having the flag as a
-        // regular mutable field inside the stage is cleaner
-
-        // Atomic boolean to track the paused state
-        AtomicBoolean isPaused = new AtomicBoolean(isPausedOnStart);
-
         interface CommandHandler {
             AsyncCallback<ValveControl.Command> getCommandHandler();
 
-            // Feedback: this is the internal method of the stage and should not be in the interface
-            void onAsyncCommand(ValveControl.Command command);
+            boolean isPaused();
         }
 
         abstract class CommandHandlerGraphStageLogic extends GraphStageLogic implements CommandHandler {
@@ -68,6 +59,8 @@ public class Valve<T> extends AbstractGraphStageWithMaterializedValue<FlowShape<
         // Akka GraphStageLogic implementation
         CommandHandlerGraphStageLogic logic = new CommandHandlerGraphStageLogic(shape) {
 
+            boolean isPaused = isPausedOnStart;
+
             private boolean pullObserved = false;
 
             private final AsyncCallback<ValveControl.Command> commandHandler =
@@ -77,56 +70,24 @@ public class Valve<T> extends AbstractGraphStageWithMaterializedValue<FlowShape<
                 return commandHandler;
             }
 
+            // TODO: If I am strictly reading if it is paused would this work?
+            public boolean isPaused() {
+                return isPaused;
+            }
+
             public void onAsyncCommand(ValveControl.Command command) {
                 switch (command) {
                     case PAUSE -> {
-                        if (!isPaused.get()) {
-                            // we pause first then empty what is in buffered so we don't
-                            // have a situation with an element not being processed
-                            isPaused.set(true);
-                            if (isAvailable(in)) {
-                                // Feedback: this is not safe, that there is an element available for pulling
-                                // does not mean the downstream is ready for pushing, when we are reacting on
-                                // an async callback.
-                                //
-                                // There are two possible ways this could be implemented:
-                                // 1. complete pause as soon as the stage sees pause command here, if there was a pull
-                                //    sent upstream, the element will be stuck in the in-buffer until unpaused
-                                // 2. if there was a pull sent upstream, the pause means no more demand is sent upstream
-                                //    but once that element arrives it will still go through
-                                //
-                                // It seems like that the second one is what you are aiming for, if the pause method
-                                // can be fire and forget, that is easy because then is just about pausing pulls, pushes
-                                // can be let through.
-                                //
-                                // If you also need to observe that the pause completed so that no more elements will
-                                // go downstream until unpaused, that is a bit trickier though. You will also need to
-                                // have pass a CompletableFuture<Done> in the command that you can either complete right
-                                // away here, if this stage has not been pushed since the last pull, or keep around
-                                // and complete once that last element comes if this stage did pull and is waiting for
-                                // a push.
-                                //
-                                // If you in addition to that also need to be able to allow multiple callers pausing and
-                                // unpausing, that complicates the whole thing quite a bit further than if only on place
-                                // is allowed to togle the state.
-                                //
-                                push(out, grab(in));
-                            }
+                        if (!isPaused) {
+                            isPaused = true;
                         }
                     }
                     case RESUME -> {
-                        if (isPaused.get()) {
+                        if (isPaused) {
                             // we unpause first so that we don't have a race condition of a downstream
                             // pulling data faster than we can resume
-                            isPaused.set(false);
-                            if (isAvailable(in)) {
-                                // Feedback: this is not safe, but should also not be needed if you have sorted always
-                                // letting elements through if upstream was pulled when the pause was triggered - then
-                                // there will _never_ be an element in here.
-                                push(out, grab(in));
-                            } else if ((isAvailable(out) && hasBeenPulled(in)) || pullObserved) {
-                                // Feedback: you should be able to know that !hasBeenPulled(in) and not check that
-                                // (not that it hurts to check, just that it should always be false)
+                            isPaused = false;
+                            if (isAvailable(out) || pullObserved) {
                                 pullObserved = false; //reset the flag
                                 pull(in);
                             }
@@ -141,14 +102,7 @@ public class Valve<T> extends AbstractGraphStageWithMaterializedValue<FlowShape<
                 setHandler(in, new InHandler() {
                     @Override
                     public void onPush() {
-                        // Feedback: as I mentioned above, for the easiest, relatively correct version that doesn't leave
-                        // a single element around in a buffer if upstream was pulled, I'd let the pause logic only be
-                        // about pulls, and always let pushes through here.
-
-                        // Push the element downstream if not paused
-                        if (!isPaused.get()) {
-                            push(out, grab(in));
-                        }
+                        push(out, grab(in));
                     }
                 });
 
@@ -156,7 +110,7 @@ public class Valve<T> extends AbstractGraphStageWithMaterializedValue<FlowShape<
                     @Override
                     public void onPull() {
                         // Pull upstream when downstream asks for data
-                        if (!isPaused.get()) {
+                        if (!isPaused) {
                             pull(in);
                         } else {
                             pullObserved = true;
@@ -168,19 +122,13 @@ public class Valve<T> extends AbstractGraphStageWithMaterializedValue<FlowShape<
 
         // Control handle for external interaction
         ValveControl control = new ValveControl() {
-            // Feedback: not sure what the gate is meant to do, but if it is a stream deathwatch or something like that
-            // I'd recommend not ad-hhoc implementing that again in this stage but rely on using watchTermination for that
-            // when composing the stream
-            private final CompletableFuture<ValveControl.Complete> gate = new CompletableFuture<>();
             private final AsyncCallback<ValveControl.Command> commandHandler = logic.getCommandHandler();
 
             @Override
-            public void pause() throws InterruptedException, TimeoutException {
-                // Feedback: never do Await and hide the blocking like this, if this was called inside an actor of from that
-                // a CompletionStage callback it causes starvation of the threadpool. If completion needs to be signalled
-                // return CompletionStage<Done>, if the caller does not need to know, just don't Await the result but do
-                // a fire and forget, in that case you can also use `invoke` instead of `invokeWithFeedback`.
-                Await.result(commandHandler.invokeWithFeedback(ValveControl.Command.PAUSE), scala.concurrent.duration.Duration.apply(1, TimeUnit.SECONDS));
+            public CompletionStage<Done> pause() throws InterruptedException, TimeoutException {
+                return scala.jdk.javaapi.FutureConverters.asJava(
+                        commandHandler.invokeWithFeedback(ValveControl.Command.PAUSE)
+                );
             }
 
             @Override
@@ -190,13 +138,7 @@ public class Valve<T> extends AbstractGraphStageWithMaterializedValue<FlowShape<
 
             @Override
             public boolean isPaused() {
-                return isPaused.get();
-            }
-
-            @Override
-            public CompletionStage<ValveControl.Complete> complete() {
-                gate.complete(ValveControl.Completed);
-                return gate;
+                return logic.isPaused();
             }
         };
 
--- src/main/java/io/example/ValveControl.java
+++ src/main/java/io/example/ValveControl.java
@@ -1,27 +1,19 @@
 package io.example;
 
+import akka.Done;
+
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeoutException;
 
 public interface ValveControl {
     // Pause the stream
-    void pause() throws InterruptedException, TimeoutException;
-
-    ;
+    CompletionStage<Done> pause() throws InterruptedException, TimeoutException;
 
     // Resume the stream
     void resume() throws InterruptedException, TimeoutException;
 
     // Check if the stream is paused
     boolean isPaused();
-
-    // Completes the valve, signaling it's finished
-    CompletionStage<Complete> complete();
-
-    public static record Complete() {
-    }
-
-    public static Complete Completed = new Complete();
 
     enum Command {
         PAUSE, RESUME
